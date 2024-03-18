6161b2cfa62efe88071b2598687d3045c4fdf784
Deep Kawale <76204769+Deepkk-9@users.noreply.github.com>
2024-03-18 22:10:00 +0530

    Tried ejecting from expo to bare(didn't work correctly)

--- RazorPay/screens/RazorpayScreen.js
+++ RazorPay/screens/RazorpayScreen.js
@@ -1,49 +1,41 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Button, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import * as Location from 'expo-location';
 
 const RazorpayScreen = () => {
-    const navigation = useNavigation();
 
-    const handlePayment = async () => {
-        try {
-            // Replace with the actual order ID obtained from your server
-            const orderId = 'order_NjXubx0LyADnHK';
+    useEffect(() => {
+        (async () => {
 
-            const apiKey = 'q6NK5sKxVG1yt60vsnrOgNZ9';
-            const encodedApiKey = encodeURIComponent(apiKey);
-
-            console.log(encodedApiKey);
-
-            // Construct the Razorpay payment URL
-            const razorpayUrl = `https://api.razorpay.com/v1/checkout/embedded?key=${encodedApiKey}&order_id=${orderId}&amount=100&name=ProSkillz&description=Plumber`;
-
-            console.log(razorpayUrl);
-
-            // Navigate to the WebView screen
-            navigation.navigate('WebView', { razorpayUrl });
-
-            const response = await fetch(razorpayUrl);
-            const responseData = await response.json();
-
-            if (response.ok) {
-                // Handle successful response
-                console.log('Razorpay Payment Response:', responseData);
-            } else {
-                // Handle error response
-                console.error('Razorpay API Error:', responseData);
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
             }
-        } catch (error) {
-            console.error('Error making Razorpay API request:', error.message);
-        }
-    };
 
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
 
     return (
         <View>
-            <Button title="Pay with Razorpay" onPress={handlePayment} />
+            <Text>{text}</Text>
         </View>
     );
-};
+}
+
+
 
 export default RazorpayScreen;
